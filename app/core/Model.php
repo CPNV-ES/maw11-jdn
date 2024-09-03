9dfc92eb58bb916402500c68d8a07cf557cbc841
David Dieperink <david.dieperink@ik.me>
2024-09-03 10:48:47 +0200

    adde comment + method in ExerciceModel

--- app/core/Model.php
+++ app/core/Model.php
@@ -25,16 +25,34 @@ class Model
         $this->db = Database::getInstance($config);
     }
 
+    /**
+     * Query method to make database queries
+     * @param mixed $query - sql query
+     * @param mixed $params - parameters of the query
+     * @return bool|PDOStatement - statement of the query
+     */
     protected function query($sql, $params = [])
     {
         return $this->db->query($sql, $params);
     }
 
+    /**
+     * Fetch method to fetch specific data from database
+     * @param mixed $query - sql query
+     * @param mixed $params - parameter of the query
+     * @return mixed - statement of the query
+     */
     protected function fetch($sql, $params = [])
     {
         return $this->db->fetch($sql, $params);
     }
 
+    /**
+     * Fetch method to fetch all data from database
+     * @param mixed $query - sql query
+     * @param mixed $params - parameter of the query
+     * @return array - statement of the query
+     */
     protected function fetchAll($sql, $params = [])
     {
         return $this->db->fetchAll($sql, $params);
--- app/models/ExerciseModel.php
+++ app/models/ExerciseModel.php
@@ -1,4 +1,15 @@
 <?php
 require_once '../core/Model.php';
 
-class ExerciseModel extends Model {}
+class ExerciseModel extends Model
+{
+    /**
+     * Method to get all exercise
+     * @return array - array of exercises 
+     */
+    public function getExercises()
+    {
+        $query = "SELECT * FROM exercises";
+        return $this->fetchAll($query);
+    }
+}
