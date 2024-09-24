a575dfe5848a8e1c29086069849a5e27d447e331
T5uy0 <julienschneider964@gmail.com>
2024-09-24 12:17:38 +0200

    feat: create home.php

---
+++ app/views/home/home.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ExerciceLooper</title>
+</head>
+
+<body>
+    <header>
+        <img src="" alt="">
+        <h1>
+            Exercice<br>Loopers
+        </h1>
+    </header>
+
+    <main>
+        <div class="boutton-container">
+            <a href="">TAKE AN EXERCISE</a>
+            <a href="">CREATE AN EXERCISE</a>
+            <a href="">MANAGE AN EXERCISE</a>
+        </div>
+    </main>
+</body>
+
+</html>
