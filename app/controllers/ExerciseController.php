fdada739c4efd00d7f42ce39f293c7a9416147a6
Nathan Chauveau <nathan.chauveau@eduvaud.ch>
2024-09-26 11:32:00 +0200

    feat: add path to page + html code (without data)

--- app/controllers/ExerciseController.php
+++ app/controllers/ExerciseController.php
@@ -4,8 +4,10 @@ require_once APP_DIR . '/core/Controller.php';
 
 class ExerciseController extends Controller
 {
+
     public function renderer($request_uri)
     {
+
         switch ($request_uri) {
             case '/exercises':
                 require_once VIEW_DIR . '/home/manage-exercise.php';
@@ -14,7 +16,7 @@ class ExerciseController extends Controller
                 require_once VIEW_DIR . '/home/manage-exercise.php';
                 exit();
             case '/exercises/answering':
-                require_once VIEW_DIR . '/home/manage-exercise.php';
+                require_once VIEW_DIR . '/home/take-exercise.php';
                 exit();
             default:
                 header("HTTP/1.0 404 Not Found");
---
+++ app/views/home/take-exercise.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/take-exercice.css">
+</head>
+
+<?php
+
+$records = [
+    [
+        'id' => 1,
+        'title' => 'Record 1 Title',
+    ],
+    [
+        'id' => 2,
+        'title' => 'Record 2 Title',
+    ],
+    [
+        'id' => 3,
+        'title' => 'Record 3 Title',
+    ],
+    [
+        'id' => 4,
+        'title' => 'Record 4 Title',
+    ]
+];
+
+?>
+
+<body>
+    <header>
+        <section>
+            <a href="/">
+                <img src="images/logo.png">
+            </a>
+        </section>
+    </header>
+    <main class="container">
+        <title>ExerciseLooper</title>
+        <ul class="ansering-list">
+            <!--TODO : foreach record -->
+            <li class="row">
+                <div class="column card">
+                    <div class="title"><!-- title --> hello</div>
+                    <a class="button" href="/exercises/*id*/fulfillments/new">Take it</a>
+                </div>
+            </li>
+        </ul>
+    </main>
+</body>
+
+</html>
