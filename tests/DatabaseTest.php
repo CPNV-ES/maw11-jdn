520e475a1d4ccb91f086e5b51b7af9a76a5da53b
David Dieperink <david.dieperink@ik.me>
2024-10-08 12:36:36 +0200

    feat: added setUp method

--- tests/DatabaseTest.php
+++ tests/DatabaseTest.php
@@ -3,4 +3,14 @@
 use PHPUnit\Framework\TestCase;
 use App\Core\Database;
 
-class DatabaseText extends TestCase {}
+class DatabaseText extends TestCase
+{
+    private $database;
+
+    public function setUp(): void
+    {
+        $config = require __DIR__ . '../../../config/config.php';
+
+        $this->database = new Database($config);
+    }
+}
