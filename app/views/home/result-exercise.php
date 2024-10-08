15643d541f2452d6b6d4c94ba50b1d55bba11172
Nathan Chauveau <nathan.chauveau@eduvaud.ch>
2024-10-08 12:12:07 +0200

    feat: add both php css pages (empty in both)

---
+++ app/views/home/result-exercise.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<meta charset="UTf-8" />
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="/css/style.css" />
+    <link rel="stylesheet" href="/css/result-exercise.css" />
+</head>
+
+<body>
+
+</body>
+
+</html>
