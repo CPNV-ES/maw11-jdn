1f6dc0d923fbb499a295ff1e2f3978132ddcbfa3
Nathan Chauveau <nathan.chauveau@eduvaud.ch>
2024-11-22 08:22:58 +0100

    fix: Revert "feat: modify DB do add a new table" because of a change in the ERM of the DB
    This reverts commit 1c814281b96bf35f89c21b67fde919a2eaeff32a.

--- db/insert_fake_data.sql
+++ db/insert_fake_data.sql
@@ -20,18 +20,12 @@ INSERT INTO fields (label, id_exercises, id_fields_type) VALUES ('Fields linked 
 INSERT INTO fields (label, id_exercises, id_fields_type) VALUES ('Fields linked to exercise 2 with type 2', 3, 2);
 INSERT INTO fields (label, id_exercises, id_fields_type) VALUES ('Fields linked to exercise 2 with type 3', 3, 3);
 
-INSERT INTO answers (value, id_fields, id_exercise_answer) VALUES ('Answer for field 1', 1, 1);
-INSERT INTO answers (value, id_fields, id_exercise_answer) VALUES ('Answer for field 2', 2, 1);
-INSERT INTO answers (value, id_fields, id_exercise_answer) VALUES ('Answer for field 3', 3, 1);
-INSERT INTO answers (value, id_fields, id_exercise_answer) VALUES ('Answer for field 4', 4, 2);
-INSERT INTO answers (value, id_fields, id_exercise_answer) VALUES ('Answer for field 5', 5, 2);
-INSERT INTO answers (value, id_fields, id_exercise_answer) VALUES ('Answer for field 6', 6, 2);
-INSERT INTO answers (value, id_fields, id_exercise_answer) VALUES ('Answer for field 7', 7, 3);
-INSERT INTO answers (value, id_fields, id_exercise_answer) VALUES ('Answer for field 8', 8, 3);
-INSERT INTO answers (value, id_fields, id_exercise_answer) VALUES ('Answer for field 9', 9, 3);
-
-INSERT INTO exercise_answer (id_exercise_answer, id_exercises, created_at) VALUES (1, 1, '2024-11-05 22:41:32 UTC');
-INSERT INTO exercise_answer (id_exercise_answer, id_exercises, created_at) VALUES (2, 2, '2024-12-07 14:35:07 UTC');
-INSERT INTO exercise_answer (id_exercise_answer, id_exercises, created_at) VALUES (3, 3, '2024-02-10 00:01:50 UTC');
-
-
+INSERT INTO answers (value, id_fields) VALUES ('Answer for field 1', 1);
+INSERT INTO answers (value, id_fields) VALUES ('Answer for field 2', 2);
+INSERT INTO answers (value, id_fields) VALUES ('Answer for field 3', 3);
+INSERT INTO answers (value, id_fields) VALUES ('Answer for field 4', 4);
+INSERT INTO answers (value, id_fields) VALUES ('Answer for field 5', 5);
+INSERT INTO answers (value, id_fields) VALUES ('Answer for field 6', 6);
+INSERT INTO answers (value, id_fields) VALUES ('Answer for field 7', 7);
+INSERT INTO answers (value, id_fields) VALUES ('Answer for field 8', 8);
+INSERT INTO answers (value, id_fields) VALUES ('Answer for field 9', 9);
