30aceed485967f2938dceff6773fe02f274b5e1b
Julien Schneider <julienschneider964@gmail.com>
2024-12-03 20:24:53 +0100

    style: add redirection on exercise title

--- app/views/home/result-field.php
+++ app/views/home/result-field.php
@@ -3,7 +3,7 @@ ob_start();
 
 $backgroundClass = 'managing';
 $cssPath = '/css/result-pages.css';
-$headTitle = "Exercise: {$exercise['title']}";
+$headTitle = "Exercise: <a class='title-exercise' href='/exercises/{$exercise['id_exercises']}/results'><strong>{$exercise['title']}</strong></a>";
 
 require_once VIEW_DIR . '/layouts/header.php';
 ?>
