e7a8d847554ce0f3878ffd38d9329e2088c5c040
Nathan Chauveau <nathan.chauveau@eduvaud.ch>
2024-12-04 11:56:58 +0100

    style: readd the ob part in fulfill-exercise.php

--- app/views/home/fulfill-exercise.php
+++ app/views/home/fulfill-exercise.php
@@ -1,65 +1,55 @@
-<!DOCTYPE html>
-<html lang="fr">
-<meta charset="UTf-8" />
+<?php
+ob_start();
 
-<head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="/css/style.css" />
-    <link rel="stylesheet" href="/css/fulfill-exercise.css" />
-</head>
+$cssPath = '/css/fulfill-exercise.css';
+$headTitle = "Exercise: {$exercise['title']}";
 
-<body>
-    <header class="heading">
-        <section class="container">
-            <a href="/">
-                <img class="header-img" src="/images/logo.png" />
-            </a>
-            <span class="exercise-title">Exercise: <b><?= $exercise['title'] ?> </b></span>
-        </section>
-    </header>
-    <main class="container">
-        <h1>Your take</h1>
+require_once VIEW_DIR . '/layouts/header.php';
+?>
+<main class="container">
+    <h1>Your take</h1>
 
-        <?php
-        $message = ($_SESSION['state'] == 'edit')
-            ? "Bookmark this page, it's yours. You'll be able to come back later to finish."
-            : "If you'd like to come back, simply submit it with blanks.";
-        ?>
+    <?php
+    $message = ($_SESSION['state'] == 'edit')
+        ? "Bookmark this page, it's yours. You'll be able to come back later to finish."
+        : "If you'd like to come back, simply submit it with blanks.";
+    ?>
 
-        <label><?= $message ?></label>
-        <form action="
+    <label><?= $message ?></label>
+    <form action="
             <?php if ($_SESSION['state'] == "new"): ?>
             /exercises/<?= $exercise['id_exercises'] ?>/fulfillments/
              <?php else: ?>
             /exercises/<?= $exercise['id_exercises'] ?>/fulfillments/<?= $exerciseAnswer['id_fulfillments'] ?>/edit
              <?php endif; ?>
             " method="post" accept-charset="UTF-8">
-            <?php if ($_SESSION['state'] == "new"): ?>
-                <input type="hidden" type="date" name="created_at" value="<?= date("Y-m-d H:i:s") ?>">
+        <?php if ($_SESSION['state'] == "new"): ?>
+            <input type="hidden" type="date" name="created_at" value="<?= date("Y-m-d H:i:s") ?>">
+        <?php else: ?>
+            <input type="hidden" type="date" name="updated_at" value="<?= date("Y-m-d H:i:s") ?>">
+        <?php endif; ?>
+        <?php $positionAnswer = 0 ?>
+        <?php foreach ($fields as $field): ?>
+
+            <h3><?= htmlspecialchars($field['label']) ?></h3>
+            <?php if ($field['id_fields_type'] == 1): ?>
+                <input type="hidden" name="idfield<?= htmlspecialchars($field['id_fields']) ?>[]" value="<?= $field['id_fields'] ?>">
+                <input type="text" name="idfield<?= htmlspecialchars($field['id_fields']) ?>[]" id="<?= htmlspecialchars($field['id_fields']) ?>" value="<?= $answers[$positionAnswer]['value'] ?? '' ?>" />
+
             <?php else: ?>
-                <input type="hidden" type="date" name="updated_at" value="<?= date("Y-m-d H:i:s") ?>">
+                <input type="hidden" name="idfield<?= htmlspecialchars($field['id_fields']) ?>[]" value="<?= $field['id_fields'] ?>">
+                <textarea id="<?= htmlspecialchars($field['id_fields']) ?>" name="idfield<?= htmlspecialchars($field['id_fields']) ?>[]" rows="4" cols="50"><?= $answers[$positionAnswer]['value'] ?? ''  ?></textarea>
+
             <?php endif; ?>
-            <?php $positionAnswer = 0 ?>
-            <?php foreach ($fields as $field): ?>
-
-                <h3><?= htmlspecialchars($field['label']) ?></h3>
-                <?php if ($field['id_fields_type'] == 1): ?>
-                    <input type="hidden" name="idfield<?= htmlspecialchars($field['id_fields']) ?>[]" value="<?= $field['id_fields'] ?>">
-                    <input type="text" name="idfield<?= htmlspecialchars($field['id_fields']) ?>[]" id="<?= htmlspecialchars($field['id_fields']) ?>" value="<?= $answers[$positionAnswer]['value'] ?? '' ?>" />
-
-                <?php else: ?>
-                    <input type="hidden" name="idfield<?= htmlspecialchars($field['id_fields']) ?>[]" value="<?= $field['id_fields'] ?>">
-                    <textarea id="<?= htmlspecialchars($field['id_fields']) ?>" name="idfield<?= htmlspecialchars($field['id_fields']) ?>[]" rows="4" cols="50"><?= $answers[$positionAnswer]['value'] ?? ''  ?></textarea>
-
-                <?php endif; ?>
-                <?php $positionAnswer = $positionAnswer + 1 ?>
-            <?php endforeach; ?>
+            <?php $positionAnswer = $positionAnswer + 1 ?>
+        <?php endforeach; ?>
 
 
 
-            <input type="submit" class="action" data-disable-with="Save">
-        </form>
-    </main>
-</body>
+        <input type="submit" class="action" data-disable-with="Save">
+    </form>
+</main>
+<?php
 
-</html>
+$slot = ob_get_clean();
+require_once VIEW_DIR . '/layouts/app-layout.php';
