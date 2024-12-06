dcb77c36c7fe616a3a00119e3c1ce47689c8925b
Julien Schneider <160748509+T5uy0@users.noreply.github.com>
2024-12-06 10:24:15 +0100

    Feature/edit field page (#46)
    * feat: add edit name and type of field
    
    * style: remove var_dump and change color of exercise name
    
    * fix: change redirection in exercise name
    
    * refactor: reorganisation and add commentary

---
+++ app/views/home/edit-field-page.php
@@ -0,0 +1,43 @@
+<?php
+ob_start();
+
+$backgroundClass = 'managing';
+$cssPath = '/css/edit-field-page.css';
+$headTitle = "Exercise: <a class='title-exercise' href='/exercises/{$exercise['id_exercises']}/fields'><strong>{$exercise['title']}</strong></a>";
+
+require_once VIEW_DIR . '/layouts/header.php';
+?>
+<div class="container">
+    <h1>Editing Field</h1>
+    
+    <form action="/exercises/<?= $exercise['id_exercises'] ?>/fields/<?= $field['id_fields'] ?>/update" method="post" accept-charset="UTF-8">
+        <div>
+            <label for="field_label">Label</label>
+            <input
+                required
+                type="text"
+                name="field_label"
+                id="field_label"
+                value="<?= $field['label'] ?>" />
+        </div>
+        <div>
+            <label for="field_type">Value kind</labe>
+            <select name="field_type" id="field_type" required>
+                <option value="1" <?= $field['id_fields_type'] == 1 ? 'selected' : '' ?>>Single line text</option>
+                <option value="2" <?= $field['id_fields_type'] == 2 ? 'selected' : '' ?>>List of single lines</option>
+                <option value="3" <?= $field['id_fields_type'] == 3 ? 'selected' : '' ?>>Multi-line text</option>
+            </select>
+        </div>
+        <div>
+            <input
+                class="button"
+                type="submit"
+                name="commit"
+                value="Edit field"
+                data-disable-with="Update Exercise" />
+        </div>
+    </form>
+</div>
+<?php
+$slot = ob_get_clean();
+require_once VIEW_DIR . '/layouts/app-layout.php';
