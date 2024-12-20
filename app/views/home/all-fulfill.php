72091429d01c7729f8c89fe46290e195d2e027f0
David Dieperink <david.dieperink@ik.me>
2024-12-20 09:43:09 +0100

    refactor: updated href link on secret page

--- app/views/home/all-fulfill.php
+++ app/views/home/all-fulfill.php
@@ -32,16 +32,16 @@ require_once VIEW_DIR . '/layouts/header.php'
                 <tr>
                     <td><?= htmlspecialchars($createAt); ?></td>
                     <td>
-                        <a href="<?= $createdAtWhidId[$createAt]; ?>">Show</a>
+                        <a href="fulfillments/<?= $createdAtWhidId[$createAt]; ?>">Show</a>
                     </td>
                     <?php foreach ($fulfillments as $fulfillment):
                         if ($fulfillment['id_exercises'] == $exercise['id_exercises'] && $fulfillment['created_at'] == $createAt):  ?>
                             <td>
-                                <a href="<?= $fulfillment['id_fulfillments'] ?>/edit">Edit</a>
+                                <a href="fulfillments/<?= $fulfillment['id_fulfillments'] ?>/edit">Edit</a>
                             </td>
 
                             <td>
-                                <a href="<?= $fulfillment['id_fulfillments'] ?>/destroy">Destroy</a>
+                                <a href="fulfillments/<?= $fulfillment['id_fulfillments'] ?>/destroy">Destroy</a>
                             </td>
                     <?php endif;
                     endforeach; ?>
